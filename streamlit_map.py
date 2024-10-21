1fb59e4c6f15ec32215dddfac3a4d38482814446
Ward Greunke <wgreunke@gmail.com>
2024-10-21 12:09:50 -0700

    moved min / max out of slider

--- streamlit_map.py
+++ streamlit_map.py
@@ -18,18 +18,26 @@ events_df['date-of-event'] = pd.to_datetime(events_df['date-of-event'])
 
 st.write(events_df)
 
+min_date = events_df['date-of-event'].min().to_pydatetime().date()
+max_date = events_df['date-of-event'].max().to_pydatetime().date()
+initial_date = events_df['date-of-event'].min().to_pydatetime().date()
+# Check if min_date and max_date are the same
+# If they are the same, add one day to max_date to create a range for the slider.
+if min_date == max_date:
+    max_date = max_date + datetime.timedelta(days=5)  # Add one day to max_date
+
 
 # Create slider with correct date range (fixed)
 selected_date = st.slider(
     'Select a date to filter events before this date',
-    min_value=events_df['date-of-event'].min(),
-    max_value=events_df['date-of-event'].max(),
-    value=events_df['date-of-event'].max(),
+    min_value=min_date,
+    max_value=max_date,
+    value=initial_date,
     format="YYYY-MM-DD"
 )
 
-
-
+#Convert to python date
+selected_date=pd.Timestamp(selected_date)
 filtered_events_df = events_df[events_df['date-of-event'] <= selected_date]
 st.write(filtered_events_df)
 
